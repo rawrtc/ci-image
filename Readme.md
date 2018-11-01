23a9eb54b6cb3563c8138a729202ea9074940a02
Lennart Grahl <lennart.grahl@gmail.com>
2018-11-02 00:22:51 +0100

    Add image for cross-compilation targeting Windows for x86

---
+++ Readme.md
@@ -0,0 +1,10 @@
+[![CircleCI build status][circleci-badge]][circleci-url]
+
+Docker image for Continuous Integration builds of RAWRTC targeting Windows for
+the x86 architecture.
+
+
+
+[circleci-badge]: https://circleci.com/gh/rawrtc/ci-image/tree/cross%2Fwindows-x86.svg?style=shield
+[circleci-url]: https://circleci.com/gh/rawrtc/ci-image/tree/cross%2Fwindows-x86
+
---
+++ windows-x86/generate-meson-cross-file.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os
+
+
+cross_file_formatter = """\
+[host_machine]
+system = 'windows'
+cpu_family = 'x86'
+cpu = 'i686'
+endian = 'little'
+
+[properties]
+root = '{CROSS_ROOT}'
+sys_root = '{CROSS_ROOT}/{CROSS_TRIPLE}'
+
+[binaries]
+c = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-gcc'
+cpp = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-cpp'
+ar = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-ar'
+as = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-as'
+ld = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-ld'
+strip = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-strip'
+pkgconfig = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-pkg-config'
+windres = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-windres'
+fortran = '{CROSS_ROOT}/bin/{CROSS_TRIPLE}-gfortran'
+exe_wrapper = 'wine'
+"""
+
+if __name__ == '__main__':
+    print(cross_file_formatter.format(**os.environ), end='')
+
